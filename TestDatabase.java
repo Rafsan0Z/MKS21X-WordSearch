629e20a9dc5df6ac6ea617eb1bbe0926db8a8b50
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-11-14 18:02:46 -0500

    All tests and their results will be stored here!

---
+++ TestDatabase.java
@@ -0,0 +1,17 @@
+// All the tests done in the terminal are stored here!
+
+//form: java WordSearch row, col, fileName, seed(Optional), key(Optional)
+
+//Test part 1
+
+//1. java WordSearch
+
+//Test part 2
+
+
+
+//Test part 3
+
+
+
+//Test part 4
