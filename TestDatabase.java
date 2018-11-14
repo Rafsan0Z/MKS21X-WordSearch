0a52abf4fcb725d7b847e1eb7c6edb3ad168e2e8
RoboticsRafsan01 <mzaman1@stuy.edu>
2018-11-14 18:32:50 -0500

    New test casses added!

--- TestDatabase.java
+++ TestDatabase.java
@@ -4,14 +4,33 @@
 
 //Test part 1
 
-//1. java WordSearch
+//1. java WordSearch 10, 10, word.txt
+//2. java WordSearch 10, 10, word.txt, 283939
+//3. java WordSearch 10, 10, word.txt, 783921
+//4. java WordSearch 10, 20, word.txt
+//5. java WordSearch 10, 10, word.txt, 763892, key
+//6. java WordSearch 10, 20, word.txt, 839201, key
 
 //Test part 2
 
-
+//1. java WordSearch
+//2. java WordSearch
+//3. java WordSearch
+//4. java WordSearch
+//5. java WordSearch
 
 //Test part 3
 
-
+//1. java WordSearch
+//2. java WordSearch
+//3. java WordSearch
+//4. java WordSearch
+//5. java WordSearch
 
 //Test part 4
+
+//1. java WordSearch
+//2. java WordSearch
+//3. java WordSearch
+//4. java WordSearch
+//5. java WordSearch
